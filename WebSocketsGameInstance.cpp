63f331f5d2c624edb81f6e609d8a2c8bb4bbc03d
ilinca01101001 <146210425+ilinca01101001@users.noreply.github.com>
2025-04-19 19:56:00 +0200

    Update WebSocketsGameInstance.cpp

--- WebSocketsGameInstance.cpp
+++ WebSocketsGameInstance.cpp
@@ -1,42 +1,56 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #include "WebSocketsGameInstance.h"
 #include "WebSocketsModule.h"
 #include "Engine/Engine.h"
 
+
+
 void UWebSocketsGameInstance::Init()
 {
 	Super::Init();
+	bIsWebSocketConnected = false;
+	ConnectWebSocket();
+}
 
+void UWebSocketsGameInstance::Shutdown()
+{
+	if (WebSocket.IsValid() && WebSocket->IsConnected())
+	{
+		WebSocket->Close();
+	}
+	Super::Shutdown();
+}
+
+void UWebSocketsGameInstance::ConnectWebSocket()
+{
 	if (!FModuleManager::Get().IsModuleLoaded("WebSockets"))
 	{
 		FModuleManager::Get().LoadModule("WebSockets");
 	}
 
-	WebSocket = FWebSocketsModule::Get().CreateWebSocket("ws://127.0.0.1:3000/ws");
+	WebSocket = FWebSocketsModule::Get().CreateWebSocket(TEXT("ws://127.0.0.1:3000/ws"));
 
-	// Check connection
-	WebSocket->OnConnected().AddLambda([]()
+	WebSocket->OnConnected().AddLambda([this]()
 		{
+			bIsWebSocketConnected = true;
 			GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Green, TEXT("Successfully connected"));
 		});
 
-	WebSocket->OnConnectionError().AddLambda([](const FString& Error)
+	WebSocket->OnConnectionError().AddLambda([this](const FString& Error)
 		{
-			GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Red, Error);
+			bIsWebSocketConnected = false;  
+			GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Red, TEXT("WebSocket Error: ") + Error);
 		});
 
-	WebSocket->OnClosed().AddLambda([](int32 StatusCode, const FString& Reason, bool bWasClean)
+	WebSocket->OnClosed().AddLambda([this](int32 StatusCode, const FString& Reason, bool bWasClean)
 		{
-			GEngine->AddOnScreenDebugMessage(-1, 15.0f, bWasClean ? FColor::Green : FColor::Red, TEXT("Connection closed: ") + Reason);
+			bIsWebSocketConnected = false;  
+			GEngine->AddOnScreenDebugMessage(-1, 15.0f, bWasClean ? FColor::Green : FColor::Red, TEXT("WebSocket closed: ") + Reason);
 		});
 
-	// Receive message and notify the Blueprint actor
 	WebSocket->OnMessage().AddLambda([this](const FString& MessageString)
 		{
-			//GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Cyan, TEXT("Received message: ") + MessageString);
+			GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Cyan, TEXT("WebSocket received: ") + MessageString);
 
-			// Check if the target actor is valid and call the Blueprint event
 			if (TargetActor)
 			{
 				UFunction* Function = TargetActor->FindFunction(FName("OnWebSocketMessageReceived"));
@@ -54,26 +68,42 @@ void UWebSocketsGameInstance::Init()
 			}
 		});
 
-	// Send message debug log
 	WebSocket->OnMessageSent().AddLambda([](const FString& MessageString)
 		{
-			GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Sent message: ") + MessageString);
+			GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("WebSocket sent: ") + MessageString);
 		});
 
 	WebSocket->Connect();
 }
 
-void UWebSocketsGameInstance::Shutdown()
-{
-	if (WebSocket.IsValid() && WebSocket->IsConnected())
-	{
-		WebSocket->Close();
-	}
-	Super::Shutdown();
-}
-
-// Function to set the target actor from Blueprints
 void UWebSocketsGameInstance::SetTargetActor(AActor* NewTargetActor)
 {
 	TargetActor = NewTargetActor;
+}
+
+/* bool UWebSocketsGameInstance::IsWebSocketConnected() const
+{
+	return bIsWebSocketConnected;
+}  */
+
+void UWebSocketsGameInstance::ReconnectWebSocket()
+{
+	if (WebSocket.IsValid())
+	{
+		WebSocket->Close();
+		WebSocket.Reset();
+	}
+	ConnectWebSocket();
+}
+
+void UWebSocketsGameInstance::SendWebSocketMessage(const FString& Message)
+{
+	if (WebSocket.IsValid() && WebSocket->IsConnected())
+	{
+		WebSocket->Send(Message);
+	}
+	else
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 10.0f, FColor::Red, TEXT("WebSocket not connected, message not sent."));
+	}
 }
